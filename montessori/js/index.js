a04719ce6746bc4cf2b68a60282e482405668cd3
niksolaz <solazzo.nicola@gmail.com>
2016-10-18 12:13:47 +0200

    simple tribute for greatest woman M.Montessori

---
+++ montessori/js/index.js
@@ -0,0 +1,6 @@
+$("document").ready(function(){
+  $("h1").addClass("animated pulse text-center ").css("color","slategrey").html("<em>Maria Montessori</em>");
+  $(".phototex").addClass("animated pulse");
+  $("h2").css("color","slategrey");
+  $("h2").addClass("animated fadeIn text-center");
+});
