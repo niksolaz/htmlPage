918ec447296ced012227048caa8eb9da7885ec2e
niksolaz <solazzo.nicola@gmail.com>
2016-10-19 18:15:22 +0200

    reseted all old code and add new code

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,24 +1,3 @@
 $(document).ready(function(){
-  $("h1").addClass("text-center").css("color","teal");
-  $("#container").stellar({
-    scrollProperty:"scroll",
-    positionProperty:"position",
-    varticalScrolling:true,
-    horizontalOffset: 0,
-    verticalOffset: 0,
-    responsive:false,
-    parallaxBackgrounds: false,
-    parallaxElements: true,
-    hideDistantElements: true,
-    hideElement: function($elem) { $elem.hide(); },
-    showElement: function($elem) { $elem.show(); },
-
-    $.stellar.positionProperty.position = {
-      setTop: function($element, newTop, originalTop) {
-        $element.css('top', newTop);
-      }
-    },
-
-
-  });
+ 
 });
